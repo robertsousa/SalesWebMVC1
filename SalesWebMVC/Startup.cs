cf189dca862b2434c43e79b406b1da0c072a4ffd
Roberto Sousa <robert6257@gmail.com>
2021-09-23 17:45:47 -0300

    SellerFormViewModel and Department select component

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -34,7 +34,9 @@ namespace SalesWebMVC
                 options.UseSqlServer(Configuration.GetConnectionString("SalesWebMVCContext")));
 
             //services.AddScoped<SeedingService>();
+            
             services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
